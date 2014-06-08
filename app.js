6e8aec4838e65f79d503b87245ab1f2bdca912db
Ram Kaniyur <mumblerandjumbler@gmail.com>
2014-06-08 17:40:11 +1000

    Magic.
    I actually use the refresh token now! The code is in the wrong file,
    though. Meh.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 
 var config = require('./config'),
-    id     = config.id,
-    secret = config.secret,
-    host   = config.host;
+id     = config.id,
+secret = config.secret,
+host   = config.host;
 
 var express  = require('express'),
 app          = express(),
 ejs          = require('ejs'),
 passport     = require('passport'),
 SBHSStrategy = require('passport-sbhs'),
-uuid         = require('node-uuid');
+uuid         = require('node-uuid'),
+request        = require('request');
 
 
 passport.serializeUser(function(user,done){done(null,user)});
@@ -18,7 +19,7 @@ passport.deserializeUser(function(user,done){done(null,user)});
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(require('cookie-parser')());
-app.use(require('express-session')({secret: secret, key: id}));
+app.use(require('express-session')({secret: secret, key: id, cookie:{maxAge:7776000000}}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -31,22 +32,58 @@ var SBHS = new SBHSStrategy({
     callbackURL: 'http://'+host+'/callback'
 },
 function(accessToken, refreshToken, profile, done) {
-    profile.accessToken = accessToken;
-    profile.refreshToken = refreshToken;
-    done(null,profile);
-});
+    var now = new Date();
+    profile.tokens = {accessToken: accessToken, refreshToken: refreshToken, expires: (new Date(now.getTime() + 3600000)).valueOf()};
+        done(null,profile);
+    });
 
 passport.use(SBHS);
 
+
+function getTokens(tokens, done) {
+    if (new Date().valueOf() < tokens.expires) {
+        done(null, tokens);
+    } else {
+
+    request.post({
+        url: 'https://student.sbhs.net.au/api/token',
+        form: {
+          refresh_token: tokens.refreshToken,
+          client_id:     id,
+          client_secret: secret,
+          grant_type:    'refresh_token'
+      }
+
+  }, function (err, response, body) {
+    if(err) return done(err);
+
+    var result = JSON.parse(body);
+
+    var now = new Date();
+    var newTokens = {accessToken: result.access_token, refreshToken: tokens.refreshToken, expires: (new Date(now.getTime() + (result.expires_in * 1000))).valueOf()};
+
+        return done(null, newTokens);
+
+    });
+
+
+
+  }
+};
+
 app.get('/', function(req,res) {
     if (req.user) {
-        SBHS.day(req.user.accessToken, function (err, o) {
+        getTokens(req.user.tokens, function(err, tokens) {
+
+         req.user.tokens = tokens;
+         SBHS.day(req.user.tokens.accessToken, function (err, o) {
             if (!err && o) {
                 res.render('index', o);
             } else {
                 res.redirect('/login');
             }
         });
+     });
     } else {
         res.render('login');
     }
