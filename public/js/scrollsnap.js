16f8c2e0ebfa621fabddd629925affdb689ab9a8
Ram Kaniyur <quadrupleslap@gmail.com>
2014-09-29 01:04:45 +1000

    Testing out some snap scrolling =)

---
+++ public/js/scrollsnap.js
@@ -0,0 +1,13 @@
+// THIRDS
+(function () {
+  var timer;
+  $(window).on("touchmove scroll", function(event) {
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      var b     = $('body')[0],
+          width = b.scrollWidth - b.clientWidth,
+          half  = Math.round(   b.scrollLeft / width   *2)/2;
+      $('body').animate({scrollLeft: width * half});
+    }, 50);
+  });
+})()
